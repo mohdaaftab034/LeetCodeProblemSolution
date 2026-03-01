92bf01934240999281632130a28206cc3225e3a4
Mohd Aaftab <mohdaaftab8630@gmail.com>
2026-03-01 21:37:38 +0530

    Add LeetCode link comment to the file
    Add LeetCode link for reference

---
+++ Queue/1017. Convert to Base -2.cpp
@@ -0,0 +1,29 @@
+// LeetCode Link - https://leetcode.com/problems/convert-to-base-2
+
+
+
+// Solution --->
+class Solution {
+public:
+    string baseNeg2(int n) {
+        if (n == 0) return "0";
+        
+        string result = "";
+        
+        while (n != 0) {
+            int remainder = n % -2;
+            n /= -2;
+            
+            // Fix negative remainder
+            if (remainder < 0) {
+                remainder += 2;
+                n += 1;
+            }
+            
+            result += to_string(remainder);
+        }
+        
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
