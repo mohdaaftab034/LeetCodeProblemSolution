7c6a5c2c41e47c408f8f21020d43cc25dbb06bed
Mohd Aaftab <mohdaaftab8630@gmail.com>
2026-03-15 17:16:15 +0530

    Create 88. Merge Sorted Array.cpp

---
+++ Binary Search and Sorting/88. Merge Sorted Array.cpp
@@ -0,0 +1,37 @@
+// LeetCode Link - https://leetcode.com/problems/merge-sorted-array/description/
+
+
+
+// ---------------------Solution-----------------------
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> ans(m+n);
+
+        int i = 0, j = 0, k = 0;
+
+        while(i < m && j < n) {
+            if(nums1[i] <= nums2[j]) {
+                ans[k] = nums1[i];
+                i++;
+            } else {
+                ans[k] = nums2[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i < m) {
+            ans[k] = nums1[i];
+            i++;
+            k++;
+        }
+
+        while(j < n) {
+            ans[k] = nums2[j];
+            j++;
+            k++;
+        }
+        nums1 = ans;
+    }
+};
