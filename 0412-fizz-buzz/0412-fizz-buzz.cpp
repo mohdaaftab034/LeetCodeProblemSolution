89ac34b9f5b9e81e20c819954b4c563a451d105b
Mohd Aaftab <mohdaaftab8630@gmail.com>
2026-08-15 20:51:49 +0530

    Time: 1 ms (54.56%), Space: 11.9 MB (18.15%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -4,15 +4,12 @@ public:
         vector<string> ans;
 
         for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                ans.push_back("FizzBuzz");
-            } else if (i % 3 == 0) {
-                ans.push_back("Fizz");
-            } else if (i % 5 == 0) {
-                ans.push_back("Buzz");
-            } else {
-                ans.push_back(to_string(i));
-            }
+            ans.push_back(
+                (i % 15 == 0) ? "FizzBuzz" :
+                (i % 5 == 0)  ? "Buzz" :
+                (i % 3 == 0)  ? "Fizz" :
+                to_string(i)
+            );
         }
 
         return ans;
