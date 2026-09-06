dfa80c74d8649f0b3cb9cc48c0cb1dcece886073
Mohd Aaftab <mohdaaftab8630@gmail.com>
2026-09-06 21:31:10 +0530

    Time: 115 ms (5.07%), Space: 14.1 MB (88.32%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& arr, int target) {
+        vector<int>ans;
+         int n = arr.size();
+         for(int i = 0; i<n-1; i++)
+         {
+              for(int j = i+1; j<n; j++)
+              {
+                if(arr[i]+arr[j] == target)
+                {
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    break;
+                }
+              }
+         }
+         return ans;
+    }
+};
